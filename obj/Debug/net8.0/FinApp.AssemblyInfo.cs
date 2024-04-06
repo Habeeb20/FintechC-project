193440dbfaab9809e6a9cde7effa3edf41b46d26
ZafrullahId <zafrullahdaprince@gmail.com>
2024-04-06 17:46:10 +0100

    repository updated

--- obj/Debug/net8.0/FinApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef77d0c9a92da8fc86b4632508705a2bb2c5d93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7ed7a9269efabfc29844f660f89b038beb7359")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
